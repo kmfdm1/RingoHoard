08b5a19bddf23fd59966d47a1fb79a2f00f66450
Manfred Andres <manfred.andres@gmail.com>
2012-08-27 09:55:12 +0200

    changes changes changes.. but not more of a wire-frame still

--- ringohoard.js
+++ ringohoard.js
@@ -21,47 +21,129 @@ exports.middleware = function hoardcache(next, app) {
         'contentTypes': /^text|xml|json|javascript/
     };
 
-    // construct the key for the cache-element-lookup
+    /**
+     * construct the key for the cache-element-lookup
+     * If app doesn't want to take control over cache-key-creation the default will be used
+     * constructing cache-keys by concatination of
+     * request.scriptName + request.pathInfo + "?" + request.queryString
+     */
     var constructKey = function (request) {
+        if (app.hoardConfig.cacheKeyFactory) {
+            return app.hoardConfig.cacheKeyFactory(request);
+        }
         return request.scriptName + request.pathInfo + "?" + request.queryString;
     };
     
-    // check if the given CacheableResponse is expired
+    /**
+     * check if the given CacheableResponse is expired
+     */
     var isExpired = function(cr) {
         return new Date().getTime() > cr.validUntil;
     };
     
-    // check if the request and response allow a gziped delivery
+    /**
+     * check if the request and response allow a gziped delivery
+     */
     var useGzip = function(request, cr) {
         return cr.status == 200 &&
                !cr.headers.content-encoding &&
-               request.headers.accept-encoding.indexOf("gzip") > -1 &&
-               cr.headers.content-type.match(app.hoardConfig.contentTypes);
+               cr.headers.content-type.match(app.hoardConfig.contentTypes) &&
+               (!request || request.headers.accept-encoding.indexOf("gzip") > -1);
+    };
+    
+    /**
+     * filters the headers of the response and returns only those which can be used
+     * for a response directly from cache
+     */
+    var filterHeaders = function(headers) {
+        if (app.hoardConfig.headerFilter) {
+            return headerFilter(headers);
+        }
+        // filter out cookies per default
+        var skipHeaders = ["cookies"];
+        var filtered = {};
+        for (var i in headers) {
+            if (skipHeaders.contains(i)) {
+               continue;
+            }
+            filtered[i] = headers[i];
+        }
+        return filtered;
     };
 
-    // return the ttl determined by the requested resource
+    /**
+     * return the ttl determined by the requested resource
+     */ 
     var getTTLforRequest = function(request, response) {
        return app.hoardConfig.defaultTTL; // FIXME: make it a lookup
     };
     
-    // return the ttl determined by the statuscode of the response
+    /** 
+     * return the ttl determined by the statuscode of the response
+     */
     var getTTLforStatusCode = function(status) {
        return app.hoardConfig.defaultTTL; // FIXME: make it a lookup
     };
+
+    /**
+     * create the basic structure of a cacheable response
+     * validUntil: millis of expire-timestamp
+     * headers: object containing all headers (except the "content-encoding: gziped" which has to be set if gziped content is used for delivery 
+     * plain: the body of the plain response
+     * gziped: the body of the gziped response
+     */
+    var createCacheableResponse = function(response, ttl) {
+        var cr = {'validUntil': new Date(new Date().getTime() + ttl * 1000),
+                 'headers': {},
+                 'plain': {}};
+        cr.headers = filterHeaders(response.headers);
+        cr.plain = response.body;
+        if (useGzip(null, response)) {
+            cr.gziped = gzip(cr.plain);
+        }
+        return cr;
+    };
     
-    // check if we can deliver gziped and return the response-object according to this findings
+    /**
+     * gzip the data given and return it as ByteString
+     */
+    var gzip = function(data) {
+        var bytes = new ByteArrayOutputStream(),
+            gzip = new GZIPOutputStream(bytes);
+        if (!(data instanceof Binary)) {
+            data = data.toByteString();
+        }
+        gzip.write(data);
+        if (bytes.size() > 1024) {
+            var zipped = bytes.toByteArray();
+            bytes.reset();
+            return new ByteString(zipped);
+        }
+        return null;
+    };
+
+    /**
+     * take the cacheableresponse and return either the plain response or the gziped
+     * response ready to return to the next in the jsgi-chain
+     */
     var serviceCacheElement = function (request, cr) {
-        if (useGzip(request, cr)) {
+        if (useGzip(request, cr) && cr.gziped && cr.gziped.body) {
+            var headers = cr.headers;
+            headers.content-encoding = 'gzip';
             return {
-                "status": 200,
-                "headers": cr.headers,
-                "body": cr.gziped.body
+                "status": cr.status,
+                "headers": headers,
+                "body": new ResponseFilter(cr.gziped.body, function(part) {
+                    return part;
+                })
             };
         }
         return {
             "status": cr.status,
             "headers": cr.headers,
-            "body": cr.plain.body
+            "body": new ResponseFilter(cr.plain.body, function(part) {
+                return part;
+            })
         };
     }
 
@@ -87,7 +169,7 @@ exports.middleware = function hoardcache(next, app) {
                 if (ttl > 0) {
                     headers.set("X-Cache", "MISS from " + request.host);
                     // FIXME: request.host .. should be something like the real hostname
-                    var cr = createCacheableResponse(response);
+                    var cr = createCacheableResponse(response, ttl);
                     ce = new Element(key, cr);
                     response = serviceCacheElement(request, cr);
                 } else {
@@ -126,13 +208,23 @@ exports.middleware = function hoardcache(next, app) {
                 return {
                     'status': 302,
                     'headers': {
-                        'location': 'http://www.orf.at/locktimeout' // FIXME: configurable
+                        'location': 'http://localhost/locktimeout' // FIXME: configurable
                     }
                 }
             }
             if (!element || element.getValue() == null) {
+                // no element in cache -> service cacheMiss
+                // FIXME: look into it how other requests may wait for this to finish and use the same response for themselve
                 return serviceCacheMiss(request, key);
             } else {
+                var cr;
+                sync(function () {
+                    cr = unwrapCacheElement(element);
+                    if (isExpired(cr)) {
+                       // touch the cachableResou
+                       cr.validUntil = new Date().getTime() + 10000;
+                    }
+                }, element);
                 var cr = element.getObjectValue().parseJSON();
                 // FIXME: synchronize.. how?
                 if (isExpired(cr) {
